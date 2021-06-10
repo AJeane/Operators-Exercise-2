e9546c5ebff99c93cf5904d4896b32e17f44a3c7
AJeane <ajeane1924@gmail.com>
2021-06-10 14:38:54 -0500

    initial commit

---
+++ Operators_Exercise_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Operators_Exercise_2
+{
+    class Program
+    {
+        public static void AreaOfCircle(int radius)
+        {
+
+            (Math.PI * radius * radius)
+
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+            {
+
+
+
+                Console.WriteLine("What is the radius of your circle");
+
+                var radius = double.Parse(Console.ReadLine());
+
+
+            }
+        }
+    }
+}
